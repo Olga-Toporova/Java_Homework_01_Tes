9b3eb68fad5dc231620e357d3a05b651648294b6
Olga Toporova <sova795@yandex.ru>
2022-12-13 05:12:20 +0500

    Выполнено задание к семинару 1

---
+++ src/Solution.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        getCount(getArr(getSize()));
+    }
+
+    public static void getCount(int[] arr) {
+        int[] newArr = cutArr(arr);
+        // System.out.println(Arrays.toString(newArr));
+        for (int i = 0; i < newArr.length; i++) {
+            int count = 0;
+            for (int num : arr) {
+                if (newArr[i] == num) {
+                    count += 1;
+                }
+            }
+            showCount(newArr[i], count);
+        }
+    }
+
+    public static void showCount(int num, int count) {
+        System.out.println("Число " + num + " встречается " + count + " раз(a)");
+    }
+
+    public static int getSize() {
+        System.out.println("Введите размер массива: ");
+        Scanner input = new Scanner(System.in);
+        int size = input.nextInt();
+        return size;
+    }
+
+    public static int[] getArr(int size) {
+        int[] arr = new int[size];
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Введите число: ");
+            int num = input.nextInt();
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static int[] cutArr(int[] arr) {
+        return Arrays.stream(arr).distinct().toArray();
+    }
+}
